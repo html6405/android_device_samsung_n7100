7d85b1ee7dc237f0ecb1a273a11e7480106bbf45
Raileanu Cosmin <comico2013@gmail.com>
2019-02-02 02:22:42 +0200

    N7100: fix restart_rild script

--- configs/restart_rild.sh
+++ configs/restart_rild.sh
@@ -1,33 +1,42 @@
 #!/system/bin/sh
 
+#set -x
+
+reset_threshold=5
+
 do_reset_radio() {
-  status=$(getprop gsm.status)
-  
-  # exit if in flight mode
-  if [ "$status" == "2" ] ; then
-    return
-  fi
-  
-  subId=$(getprop gsm.subid)
-  resetCount=$($BB getprop gsm.resetcount)
+  resetCount=$(getprop gsm.resetcount)
   if [ -z $resetCount ] ; then
     resetCount=0
   fi
-  
+
+  # Allow up to one radio reset in airplane mode
+  airplane_mode=$(settings get global airplane_mode_on)
+  if [ "$airplane_mode" == "1" ] && [ $resetCount -ge 1 ] ; then
+    return;
+  fi
+
+  # No radio after several resets?
+  # Probably something goes wrong, we have nothing to do.
+  if [ $resetCount -ge $reset_threshold ] ; then
+    return
+  fi
+
+  #status=$(getprop gsm.status)
+
+  subId=$(getprop gsm.subid)
+
   if [ "$subId" != "1" ] ; then
     sleep 10
   fi
 
-  if ! test -f /tmp/.gsmreset.lock ; then
-    touch /tmp/.gsmreset.lock
-    if [ "$subId" != "1" ] ; then
-      stop ril-daemon
-      sleep 5
-      start ril-daemon
-	  setprop gsm.resetcount $(( $resetCount + 1 ))
-    fi
-	rm -f /tmp/.gsmreset.lock
+  if [ "$subId" != "1" ] ; then
+    stop ril-daemon
+    sleep 5
+    start ril-daemon
+    setprop gsm.resetcount $(( $resetCount + 1 ))
   fi
+
   setprop gsm.radioreset false
 }
 
