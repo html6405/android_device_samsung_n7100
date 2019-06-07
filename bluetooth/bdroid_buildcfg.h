5beec9a3b72398fa529eadcf4245a8b67325cd29
ComicoTeam <comico_work@outlook.com>
2019-06-07 19:13:53 +0300

    Disable BLE
    Change-Id: I4b49a0c788d11b7c64996a319559a9224ebf81dc

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -23,7 +23,4 @@
 /* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 
-#define BLE_INCLUDED TRUE
-#define BTA_GATT_INCLUDED TRUE
-#define SMP_INCLUDED TRUE
 #endif
