7b554bd7e4663d2af9e6110eb5b8ff82bd6891d5
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-10-20 14:44:53 +0200

    BoardConfig.mk: gps: add libdmitry shim for most recent gpsd.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,7 +58,8 @@ TARGET_INIT_VENDOR_LIB := libinit_n7100
 endif
 
 TARGET_LD_SHIM_LIBS += \
-    /vendor/bin/gpsd|libsamsung_symbols.so
+	/vendor/bin/gpsd|libsamsung_symbols.so \
+    /vendor/bin/gpsd|libdmitry.so
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/samsung/n7100/selinux/vendor
