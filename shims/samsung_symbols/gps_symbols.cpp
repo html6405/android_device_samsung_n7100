2c2e73cd3441c089e3b2751b906b1782246b9d88
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-09-29 15:04:19 +0200

    n7100: gps_symbols.cpp: shim missing symbols to make gpsd happy

--- shims/samsung_symbols/gps_symbols.cpp
+++ shims/samsung_symbols/gps_symbols.cpp
@@ -41,6 +41,23 @@ extern "C" void _ZNK7android16SensorEventQueue12setEventRateEPKNS_6SensorEx(){}
 extern "C" void _ZNK7android16SensorEventQueue13disableSensorEPKNS_6SensorE();
 extern "C" void _ZNK7android16SensorEventQueue13disableSensorEPKNS_6SensorE(){}
 
+extern "C" void _ZN7android8String16aSEOS0_();
+extern "C" void _ZN7android8String16aSEOS0_(){}
+
+extern "C" void _ZN7android8String16C1EOS0_();
+extern "C" void _ZN7android8String16C1EOS0_(){}
+
+extern "C" void _ZN7android9SingletonINS_13SensorManagerEE5sLockE();
+extern "C" void _ZN7android9SingletonINS_13SensorManagerEE5sLockE(){}
+
+extern "C" void _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE();
+extern "C" void _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE(){}
+
+extern "C" void _ZN7android13SensorManager16createEventQueueEv();
+extern "C" void _ZN7android13SensorManager16createEventQueueEv(){}
+
+extern "C" void _ZN7android13SensorManagerC1Ev();
+extern "C" void _ZN7android13SensorManagerC1Ev(){}
 
 extern "C" {
 
